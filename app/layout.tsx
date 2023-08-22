394e8cca2b91d61f3e5cb4373938afb41e5145bd
kmdshojib <kmd.shojib@gmail.com>
2023-08-22 15:34:49 +0600

    store has been successfully set up for redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import Providers from "@/redux/provider";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Home',
-  description: 'This is a tech E-Commerce website.',
-}
+  title: "Home",
+  description: "This is a tech E-Commerce website.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
