1c8c1900a543ce84f15932100f6ae64b6480c6f8
kmdshojib <kmd.shojib@gmail.com>
2023-08-27 00:10:31 +0600

    logger has been added for the redux store

--- redux/Store.ts
+++ redux/Store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import logger from "redux-logger";
 export const store = configureStore({
   reducer: {},
-  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
