55f7dfeeba631d5c4d547f7ed3d16218a8150e66
kmdshojib <kmd.shojib@gmail.com>
2023-08-29 16:02:07 +0600

    token successfully stored

--- redux/Store.ts
+++ redux/Store.ts
@@ -3,9 +3,11 @@ import logger from "redux-logger";
 import { combineReducers } from "redux";
 import { apiService } from "./api/api";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import authReducer from "./features/authSlice";
 
 const reducer = combineReducers({
   [apiService.reducerPath]: apiService.reducer,
+  auth: authReducer
 });
 
 const store = configureStore({
