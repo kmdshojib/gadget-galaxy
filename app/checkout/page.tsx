c98d5d8cfb9d0de51e16124afb54995f5b69f21e
kmdshojib <kmd.shojib@gmail.com>
2023-10-23 01:37:10 +0600

    payemnt has been created

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,28 +1,44 @@
 "use client";
-import { Elements, CardElement, PaymentElement } from "@stripe/react-stripe-js";
+import { Elements, CardElement } from "@stripe/react-stripe-js";
 
 import { loadStripe } from "@stripe/stripe-js";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import CheckoutForm from "../Components/checkoutform/CheckoutForm";
 
 const stripe = loadStripe(`${process.env.stripe_key}`);
-const Checkout = () => {
+const Checkout: React.FC = () => {
+  const [clientSecret, setClientSecret] = useState("");
+  useEffect(() => {
+    console.log("Setting client secret");
+    // const axios =
+    fetch("http://localhost:5000/api/v1/laptop/payment", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ price: 100 }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("Setting client secret");
+        return res.json();
+      })
+      .then((data) => setClientSecret(data.clientSecret))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   const options = {
-    clientSecret: `{{${process.env.stripe_secretkey}}}`,
+    clientSecret,
   };
-  const clientSecret ={clientSecret:`{{${process.env.stripe_secretkey}}}`}
   return (
     <div>
-      <Elements stripe={stripe} options={clientSecret}>
-      <form>
-          <div>
-            {/* <PaymentElement /> */}
- 
-            <button type="button" className="pay-button">
-              Checkout
-            </button>
-          </div>
-        </form>
-      </Elements>
+      {clientSecret && (
+        <Elements options={options} stripe={stripe}>
+          <CheckoutForm clientSecret={clientSecret} />
+        </Elements>
+      )}
     </div>
   );
 };
