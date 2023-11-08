6e60d19b37b6edea049102ed0fecb957b2afce5f
kmdshojib <kmd.shojib@gmail.com>
2023-11-09 02:18:29 +0600

    next confing modified

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "avatars.githubusercontent.com",
-            "lh3.googleusercontent.com",
-            "res.cloudinary.com",
-            "images.pexels.com"
-        ]
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
     },
     env: {
         stripe_key: process.env.stripe_key,
